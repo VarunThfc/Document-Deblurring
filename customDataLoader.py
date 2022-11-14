10d2b857995e3226a72fb41951c25875aadc0088
VarunThfc <varun.kaundinya13@gmail.com>
2022-11-14 17:38:19 -0600

    second message

--- customDataLoader.py
+++ customDataLoader.py
@@ -5,12 +5,27 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt
+import datetime
+
+def main():
+    dataset = CustomDataset()
+    print(dataset)
+
+    data_loader = DataLoader(dataset, batch_size=32, shuffle=False, num_workers = 4)
+    now = datetime.datetime.now()
+    for imgs in data_loader:
+        plt.imshow(imgs[0][0].permute(1, 2, 0))
+        print(imgs[0][0].permute(1, 2, 0).shape);
+        print(imgs[1][0].permute(1, 2, 0).shape);
+
+        plt.show()
+        plt.imshow(imgs[1][0].permute(1, 2, 0))
+        plt.show()
+        continue
+    then = datetime.datetime.now();
+    delta = then - now
+    print(delta, delta.total_seconds())
 
 
-dataset = CustomDataset()
-print(dataset)
-
-data_loader = DataLoader(dataset, batch_size=300, shuffle=True)
-
-for imgs in data_loader:
-    break
+if __name__ == "__main__":
+    main()
--- customDataSet.py
+++ customDataSet.py
@@ -4,9 +4,11 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
 import json
+import random
 
 
 class CustomDataset(Dataset):
+    listOfTransformations = [lambda img, ksize : cv2.GaussianBlur(img,(5,5),0), lambda img, ksize : cv2.medianBlur(img,5), lambda img, ksize : cv2.blur(img,5)]
     def __init__(self):
         self.imgs_path = "./publaynet/" ##Base path
         file_list = glob.glob(self.imgs_path + "*") ##Contents inside Path
@@ -19,7 +21,7 @@ class CustomDataset(Dataset):
                     self.data.append(img_path) ##Check this once we have the data
         print('size', len(self.data))
         #self.class_map = {"dogs" : 0, "cats": 1}
-        self.img_dim = (416, 416) ## Reszie dimension experiment
+        self.img_dim = (800, 800) ## Reszie dimension experiment
     
     def __len__(self):   ##Mandatory override criteria
         return len(self.data)
@@ -41,13 +43,29 @@ class CustomDataset(Dataset):
                 validFileNames.add(images['file_name'])
         print("fileName", len(validFileNames))
         return validFileNames
+    
+    def apply(self,img_to_blur, num):
+        for i in range(0,num):
+            img_to_blur = self.listOfTransformations[i](img_to_blur,5)
+        return img_to_blur
         
     def __getitem__(self, idx):
-        img_path = self.data[idx]   ##THink of modding if required
+        img_path = self.data[idx]   
         img = cv2.imread(img_path)
+        img_to_blur = cv2.imread(img_path)
         img = cv2.resize(img, self.img_dim) ##Should I just return or resize and takecare in transforms
-        #class_id = self.class_map[class_name] ##Is this required ??
-        img_tensor = torch.from_numpy(img) ##Convert to tensore
+        img_to_blur = cv2.resize(img, self.img_dim)
+        r1 = random.randint(0, 2)
+        img_to_blur = self.apply(img_to_blur, r1)
+        
+        img_tensor = torch.from_numpy(img) ##Convert to tensor
+        img_to_blur = torch.from_numpy(img_to_blur) ##Convert to tensore
+
         img_tensor = img_tensor.permute(2, 0, 1) ##SWAP channel to adhere to torch
+        img_to_blur = img_to_blur.permute(2, 0, 1) 
         #class_id = torch.tensor([class_id])
-        return img_tensor
+        return img_tensor , img_to_blur
+    
+    
+            
+        
