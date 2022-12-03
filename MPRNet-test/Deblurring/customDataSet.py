d582a2b89327d932cd1d783ca0bd434d0de609f4
Apoorva Kumar <akumar255@wisc.edu>
2022-12-03 14:04:33 -0600

    Added focus on motion blur

--- MPRNet-test/Deblurring/customDataSet.py
+++ MPRNet-test/Deblurring/customDataSet.py
@@ -32,8 +32,8 @@ def motion_blur(img, ksize):
     kernel = rotate(kernel, random_angle)
     return cv2.filter2D(img, -1, kernel)
 
-def random_blur(img_to_blur, list_of_blurs):
-    pickblur = TF.RandomChoice(list_of_blurs)
+def random_blur(img_to_blur, list_of_blurs,p=None):
+    pickblur = TF.RandomChoice(list_of_blurs,p=p)
     for _ in range(np.random.randint(1,3)):
         ksize = np.random.randint(2,4)
         img_to_blur = pickblur(img_to_blur,2*ksize-1)
@@ -64,8 +64,9 @@ class CustomDataset(Dataset):
         self.listOfBlurs = [
                         lambda img, ksize : cv2.GaussianBlur(img,(ksize,ksize),cv2.BORDER_DEFAULT),
                         lambda img, ksize : cv2.blur(img,(ksize,ksize)),
-                        lambda img, ksize : motion_blur(img, 2*ksize+1)
-                    ]## Reszie dimension experiment
+                        lambda img, ksize : motion_blur(img, 2*ksize+np.random.choice([1,-1]))
+                    ]## Resize dimension experiment
+        self.p = [1,1,10]
     
     def __len__(self):   ##Mandatory override criteria
         return len(self.data)       
@@ -74,7 +75,7 @@ class CustomDataset(Dataset):
         img_path = self.data[idx]   
         img = Image.open(img_path)
         img = self.transforms(img)
-        blurimg = Image.fromarray(random_blur(np.asarray(img),self.listOfBlurs))
+        blurimg = Image.fromarray(random_blur(np.asarray(img),self.listOfBlurs,self.p))
         img = self.totensor(img)
         blurimg = self.totensor(blurimg)
         return blurimg, img, os.path.splitext(os.path.split(img_path)[-1])[0]
